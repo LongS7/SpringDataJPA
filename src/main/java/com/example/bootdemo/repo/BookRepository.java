b326b0c21ed086def9a95e15bf2fdbf75ed44565
longs7 <longkeyvic1@gmail.com>
2021-09-13 14:02:05 +0700

    test on native_branch

--- src/main/java/com/example/bootdemo/repo/BookRepository.java
+++ src/main/java/com/example/bootdemo/repo/BookRepository.java
@@ -2,9 +2,21 @@ package com.example.bootdemo.repo;
 
 import com.example.bootdemo.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    @Query(
+            value = "select * from book",
+            nativeQuery = true
+    )
+    List<Book> selectInNative();
 
+    @Query(
+            value = "delete * from book",
+            nativeQuery = true
+    )
+    void deleteInNative();
 }
--- src/test/java/com/example/bootdemo/BootdemoApplicationTests.java
+++ src/test/java/com/example/bootdemo/BootdemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.bootdemo;
 
+import com.example.bootdemo.entity.Book;
+import com.example.bootdemo.repo.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BootdemoApplicationTests {
+	@Autowired
+	private BookRepository bookRepository;
+
+	//Test on native branch
 
 	@Test
-	void contextLoads() {
+	void testSelect() {
+		System.out.println(bookRepository.selectInNative());
 	}
 
 }
