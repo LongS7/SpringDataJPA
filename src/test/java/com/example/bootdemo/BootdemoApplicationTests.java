b88d2cfb8a1ed65f797d740d3f43631088cd82f6
longs7 <longkeyvic1@gmail.com>
2021-09-13 13:25:36 +0700

    test on repo_branch

--- src/test/java/com/example/bootdemo/BootdemoApplicationTests.java
+++ src/test/java/com/example/bootdemo/BootdemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.bootdemo;
 
+import com.example.bootdemo.entity.Book;
+import com.example.bootdemo.repo.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BootdemoApplicationTests {
+	@Autowired
+	private BookRepository bookRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testSave() {
+		Book book = new Book("Art of Coding", "JJJ", 120);
+		bookRepository.save(book);
+	}
 }
