82f07c81940cd9a725bfeba7a94e7f9b8b47e6d3
longs7 <longkeyvic1@gmail.com>
2021-09-13 14:09:24 +0700

    test on jpql_branch

--- src/main/java/com/example/bootdemo/repo/BookRepository.java
+++ src/main/java/com/example/bootdemo/repo/BookRepository.java
@@ -2,9 +2,13 @@ package com.example.bootdemo.repo;
 
 import com.example.bootdemo.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    //In JPQL
 
+    @Query("from Book")
+    List<Book> selectBookInJPQL();
 }
--- src/test/java/com/example/bootdemo/BootdemoApplicationTests.java
+++ src/test/java/com/example/bootdemo/BootdemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.bootdemo;
 
+import com.example.bootdemo.repo.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BootdemoApplicationTests {
+	//Test on JPQL branch------
+
+	@Autowired
+	private BookRepository repository;
 
 	@Test
-	void contextLoads() {
+	void testSelect() {
+		System.out.println(repository.selectBookInJPQL());
 	}
-
 }
